111b2b047f1b0d226b1acdfb381d339bdd0e17fe
112madgamer <ryleegeorge1@gmail.com>
2018-11-15 15:00:57 +1100

    Add edeleteclaim

---
+++ src/main/java/_112/griefpreventionextras/Commands/DeleteClaims.java
@@ -0,0 +1,12 @@
+package _112.griefpreventionextras.Commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class DeleteClaims implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
+        return false;
+    }
+}
