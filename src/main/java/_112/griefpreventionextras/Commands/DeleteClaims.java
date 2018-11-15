0fef719650bdbfccdb67f49fab1c917c7f18b44d
112madgamer <ryleegeorge1@gmail.com>
2018-11-15 16:29:14 +1100

    Add deleting claims in a WE section

--- src/main/java/_112/griefpreventionextras/Commands/DeleteClaims.java
+++ src/main/java/_112/griefpreventionextras/Commands/DeleteClaims.java
@@ -1,12 +1,40 @@
 package _112.griefpreventionextras.Commands;
 
+import _112.griefpreventionextras.Util.Region;
+import com.boydti.fawe.object.FawePlayer;
+import com.boydti.fawe.util.TaskManager;
+import me.ryanhamshire.GriefPrevention.Claim;
+import me.ryanhamshire.GriefPrevention.GriefPrevention;
+import org.bukkit.Bukkit;
+import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.scheduler.BukkitRunnable;
 
 public class DeleteClaims implements CommandExecutor {
     @Override
-    public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
-        return false;
+    public boolean onCommand(CommandSender sender, Command command, String s, String[] strings) {
+        if(sender instanceof Player) {
+            FawePlayer fawePlayer = FawePlayer.wrap(sender);
+            World world = Bukkit.getWorld(fawePlayer.getWorld().getName());
+            Region region = new Region(fawePlayer.getSelection().getMinimumPoint(), fawePlayer.getSelection().getMaximumPoint(), world);
+            //TODO fix async WE warning?
+            TaskManager.IMP.async(new BukkitRunnable() {
+                @Override
+                public void run() {
+                    for(Claim claim : GriefPrevention.instance.dataStore.getClaims()){
+                        if(region.locationIsInRegion(claim.getLesserBoundaryCorner()) && region.locationIsInRegion(claim.getGreaterBoundaryCorner())){
+                            fawePlayer.sendMessage("Deleting claim " + claim.getID());
+                            GriefPrevention.instance.dataStore.deleteClaim(claim);
+                        }
+                    }
+                }
+            });
+
+
+        }
+        return true;
     }
 }
---
+++ src/main/java/_112/griefpreventionextras/Util/Region.java
@@ -0,0 +1,36 @@
+package _112.griefpreventionextras.Util;
+
+import com.sk89q.worldedit.Vector;
+import org.bukkit.Location;
+import org.bukkit.World;
+
+import java.util.UUID;
+
+public class Region {
+
+    private UUID worldUniqueId;
+
+    private double maxX;
+    private double maxZ;
+
+    private double minX;
+    private double minZ;
+
+
+    public Region(Vector firstPoint, Vector secondPoint, World world) {
+        worldUniqueId = world.getUID();
+
+        maxX = Math.max(firstPoint.getBlockX(), secondPoint.getX());
+        maxZ = Math.max(firstPoint.getBlockZ(), secondPoint.getBlockZ());
+
+        minX = Math.min(firstPoint.getBlockX(), secondPoint.getBlockX());
+        minZ = Math.min(firstPoint.getBlockZ(), secondPoint.getBlockZ());
+    }
+
+    public boolean locationIsInRegion(Location loc) {
+        return loc.getWorld().getUID().equals(worldUniqueId)
+                && loc.getBlockX() > minX && loc.getBlockX() < maxX
+                && loc.getBlockZ() > minZ && loc.getBlockZ() < maxZ;
+    }
+
+}
