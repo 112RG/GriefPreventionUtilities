a5ff0a42b05d3358034fc7ec08799f9d57bb4486
112madgamer <ryleegeorge1@gmail.com>
2018-11-21 00:58:57 +1100

    Update GriefPreventionUtilities.java

--- src/main/java/_112/griefpreventionutilities/GriefPreventionUtilities.java
+++ src/main/java/_112/griefpreventionutilities/GriefPreventionUtilities.java
@@ -1,5 +1,5 @@
 package _112.griefpreventionutilities;
-//test
+//22
 import _112.griefpreventionutilities.Commands.*;
 import _112.griefpreventionutilities.Events.ClaimExpire;
 import com.boydti.fawe.util.TaskManager;
